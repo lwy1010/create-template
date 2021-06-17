5c59563f43f7d64856e4b5a52dda777547ef44b1
lwy1010 <im.leewy@gmail.com>
2021-06-17 22:57:50 +0800

    feat(h5): browser adaptation

---
+++ template-h5-ts/postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: {
+    // check out https://github.com/evrone/postcss-px-to-viewport to get more details
+    'postcss-px-to-viewport': {
+      viewportWidth: 375,
+    },
+  },
+};
