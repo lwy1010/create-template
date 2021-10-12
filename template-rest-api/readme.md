d81c6eeb258293816d58669e1d8500cc60a989b8
lwy1010 <im.leewy@gmail.com>
2021-10-12 21:00:28 +0800

    feat(rest-api): use mongodb clusterinstead

--- template-rest-api/readme.md
+++ template-rest-api/readme.md
@@ -1,1 +1,5 @@
 # template-rest-api
+
+## 环境变量
+
+本应用的环境变量管理基于[config](https://www.npmjs.com/package/config)这个库，对于一些如密码等比较机密的环境变量，可以在 config/custom-environment-variables.json 文件下进行映射，通过 export 或 set 命令设置环境变量后，即可在代码中通过 config 提供的 get() 方法读取到，更多详情请移步[config](https://www.npmjs.com/package/config)。
--- template-rest-api/src/app.ts
+++ template-rest-api/src/app.ts
@@ -8,7 +8,10 @@ import error from "@/middleware/error";
 
 const app = express();
 const port = process.env.PORT || config.get("port");
-const db = config.get("db") as string;
+const dbName = config.get("dbName");
+const dbAdmin = config.get("dbAdmin");
+const password = config.get("password");
+const url = `mongodb+srv://${dbAdmin}:${password}@template-rest-api.cme1h.mongodb.net/${dbName}?retryWrites=true&w=majority`;
 
 import postRouter from "@/routes/post";
 import userRouter from "@/routes/user";
@@ -24,11 +27,8 @@ app.use("/post", postRouter);
 app.use("/user", userRouter);
 
 async function start() {
-  await mongoose.connect(db, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-  console.log(`Connected to ${db}...`);
+  await mongoose.connect(url);
+  console.log(`Connected to ${dbName}...`);
   app.listen(port, () => {
     console.log(`Listening on port ${port}...`);
   });
