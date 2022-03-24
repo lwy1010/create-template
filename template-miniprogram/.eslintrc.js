5033e1355d08f304001635e31d355fce3c01e7e8
leewy <im.leewy@gmail.com>
2022-03-25 00:30:24 +0800

    feat(miniprogram): switch to typescript

---
+++ template-miniprogram/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: ["@7inch/eslint-config-base"],
+  globals: {
+    wx: true,
+    App: true,
+    Page: true,
+    getCurrentPages: true,
+    getApp: true,
+    Component: true,
+    requirePlugin: true,
+    requireMiniProgram: true,
+  },
+};
---
+++ template-miniprogram/prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  overrides: [
+    {
+      files: "*.wxml",
+      options: {
+        parser: "html",
+      },
+    },
+  ],
+};
